f1001013d5210b3752d8d9d097d614414500b1cc
Björn Wilmsmann <bjoern@wilmsmann.de>
2017-04-07 14:44:09 +0200

    Seed project generated via Spring Initializr

---
+++ src/main/java/de/workshops/SpringBootSeedApplication.java
@@ -0,0 +1,12 @@
+package de.workshops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSeedApplication.class, args);
+	}
+}
