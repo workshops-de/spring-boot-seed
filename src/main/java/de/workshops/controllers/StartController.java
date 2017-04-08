88fcac06db3a9be6167c2436d9fdb941d8a7c015
Björn Wilmsmann <bjoern@wilmsmann.de>
2017-04-08 17:49:02 +0200

    Fifth assignment (Add CORS headers)

--- src/main/java/de/workshops/controllers/StartController.java
+++ src/main/java/de/workshops/controllers/StartController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -24,6 +25,7 @@ public class StartController {
         return "start";
     }
 	
+	@CrossOrigin(origins = "http://localhost:4200")
 	@RequestMapping("/books")
 	@ResponseBody
     public List<Book> books(Model model) {
