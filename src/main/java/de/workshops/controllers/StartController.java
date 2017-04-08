3ded5ec3679c20a24c5d2b75479bff3a3bfcc20d
Björn Wilmsmann <bjoern@wilmsmann.de>
2017-04-08 17:22:31 +0200

    Fourth assignment (Provide a Rest-API)

--- src/main/java/de/workshops/controllers/StartController.java
+++ src/main/java/de/workshops/controllers/StartController.java
@@ -1,10 +1,14 @@
 package de.workshops.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import de.workshops.model.Book;
 import de.workshops.services.BookService;
 
 @Controller
@@ -18,5 +22,11 @@ public class StartController {
 		model.addAttribute("books", bookService.getBooks());
 		
         return "start";
+    }
+	
+	@RequestMapping("/books")
+	@ResponseBody
+    public List<Book> books(Model model) {
+		return bookService.getBooks();
     }
 }
