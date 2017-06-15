724c051bea464b12708941c78e778c1881db13b3
Björn Wilmsmann <bjoern@wilmsmann.de>
2017-06-15 12:31:16 +0200

    Added exception class

---
+++ src/main/java/de/workshops/model/ItemNotFoundException.java
@@ -0,0 +1,10 @@
+package de.workshops.model;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason="Item doesn't exist")
+public class ItemNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = -3801138054894136097L;
+}
