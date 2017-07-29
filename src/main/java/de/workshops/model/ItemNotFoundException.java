06cc6950ccc626a1f533d53ef24ed4738d219bd7
Björn Wilmsmann <bjoern@wilmsmann.de>
2017-07-29 12:00:19 +0200

    Configuration

--- src/main/java/de/workshops/model/ItemNotFoundException.java
+++ src/main/java/de/workshops/model/ItemNotFoundException.java
@@ -5,6 +5,5 @@ import org.springframework.http.HttpStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason="Item doesn't exist")
 public class ItemNotFoundException extends RuntimeException {
-
-    private static final long serialVersionUID = -3801138054894136097L;
+	
 }
