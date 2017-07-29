a51afb3e29cf8e57623dded43b5a6bfeea967890
Björn Wilmsmann <bjoern@wilmsmann.de>
2017-07-29 12:00:39 +0200

    Configuration

--- src/main/java/de/workshops/model/ItemNotFoundException.java
+++ src/main/java/de/workshops/model/ItemNotFoundException.java
@@ -5,5 +5,5 @@ import org.springframework.http.HttpStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason="Item doesn't exist")
 public class ItemNotFoundException extends RuntimeException {
-	
+
 }
