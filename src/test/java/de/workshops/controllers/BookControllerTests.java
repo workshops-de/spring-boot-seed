86c0caab1421d2b4c03f9975a486df3ae99de19a
Björn Wilmsmann <bjoern@wilmsmann.de>
2017-07-29 14:25:44 +0200

    Configuration

--- src/test/java/de/workshops/controllers/BookControllerTests.java
+++ src/test/java/de/workshops/controllers/BookControllerTests.java
@@ -5,12 +5,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.context.WebApplicationContext;
 
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.webAppContextSetup;
 import static org.hamcrest.CoreMatchers.equalTo;
 
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
 import static io.restassured.module.mockmvc.RestAssuredMockMvc.standaloneSetup;
+import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.authenticated;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -19,30 +24,40 @@ public class BookControllerTests {
 	@Autowired
 	private BookController bookController;
 
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	@Autowired
+	private FilterChainProxy filterChainProxy;
+
 	@Before
 	public void setup() {
-		standaloneSetup(bookController);
+		webAppContextSetup(webApplicationContext);
 	}
 
 	@Test
+	@WithMockUser(roles="ADMIN")
 	public void testBookList() throws Exception {
 		given()
 			.contentType("application/json;charset=UTF-8")
 		.when()
 			.post("/api/books")
 		.then()
+			.expect(authenticated().withUsername("user"))
 			.statusCode(200)
 			.body("size()", equalTo(3))
 			.body("get(0).title", equalTo("Design Patterns"));
 	}
 	
 	@Test
+	@WithMockUser(roles="ADMIN")
 	public void testBookDetail() throws Exception {
 		given()
 			.contentType("application/json;charset=UTF-8")
 		.when()
 			.post("/api/book/0")
 		.then()
+			.expect(authenticated().withUsername("user"))
 			.statusCode(200)
 			.body("title", equalTo("Design Patterns"));
 	}
